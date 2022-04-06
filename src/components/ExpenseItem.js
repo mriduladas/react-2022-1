5bb254597301ab8c8679811707cf6924483f75d4
mriduladas <mridula199@gmail.com>
2022-04-06 18:37:12 +0530

    Pass Parameter using Props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,16 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
+function ExpenseItem(props) {
 
-    const expenseDate = new Date(2022, 2,28);
-    const expenseTitle = 'Car Inurance';
-    const expenseAmount = 294;
+    // const expenseDate = new Date(2022, 2,28);
+    // const expenseTitle = 'Car Inurance';
+    // const expenseAmount = 294;
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString().substring(0,10)}</div>
+      <div>{props.date.toISOString().substring(0,10)}</div>
       <div className="expense-item__description">
-        <h2 >{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2 >{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
