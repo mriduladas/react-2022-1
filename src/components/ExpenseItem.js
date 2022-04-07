3d3ae3f702b8fa2f3ab2da91e6f738d8fff7af14
mriduladas <mridula199@gmail.com>
2022-04-07 08:08:42 +0530

    Create new Componenet for Date Formatting

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,4 +1,6 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
+
 
 function ExpenseItem(props) {
 
@@ -7,7 +9,7 @@ function ExpenseItem(props) {
     // const expenseAmount = 294;
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString().substring(0,10)}</div>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2 >{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
