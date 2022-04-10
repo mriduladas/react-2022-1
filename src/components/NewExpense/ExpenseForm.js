b81090776930e20d3512d4df71a3717aebcdc421
mriduladas <mridula199@gmail.com>
2022-04-10 15:41:00 +0530

    Renamed SetState Method Names

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,21 +2,21 @@ import { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-const [enteredTitle, setTitle] = useState('');
-const [enteredAmount, setAmount] = useState('');
-const [enteredDate, setDate] = useState('');
+const [enteredTitle, setEnteredTitle] = useState('');
+const [enteredAmount, setEnteredAmount] = useState('');
+const [enteredDate, setEnteredDate] = useState('');
 
 const titleChangeHandler = (event) => {
     console.log(event.target.value);
-    setTitle(event.target.value)
+    setEnteredTitle(event.target.value)
 }
 
 const amountChangeHandler = (event) => {
-    setAmount(event.target.value)
+    setEnteredAmount(event.target.value)
 }
 
 const dateChangeHandler = (event) => {
-    setDate(event.target.value)
+    setEnteredDate(event.target.value)
 }
 
   return (
